385fa1e5d61892a47e37f4a783a65e35fd9a9983
yp2020 <3522373084@qq.com>
2024-06-11 23:08:45 +0800

    clean up code

--- app/server.go
+++ app/server.go
@@ -30,7 +30,7 @@ func Handler(conn net.Conn) {
 		fmt.Println("Error reading request.", err.Error())
 		return
 	}
-	fmt.Printf("Request: %s %s \n", request.Method, request.URL.Path)
+	//fmt.Printf("Request: %s %s \n", request.Method, request.URL.Path)
 	if request.Method == "POST" {
 		handlerPostRequest(request, conn)
 	} else if request.Method == "GET" {
@@ -44,7 +44,7 @@ func handlerPostRequest(request *http.Request, conn net.Conn) {
 	path := request.URL.Path
 	if strings.Contains(path, "/files/") {
 		fileName := strings.TrimPrefix(path, "/files/")
-		//// 测试情况下 dir 设置为当前目录 win computer so
+		//// 测试情况下 dir 设置为当前目录 win 用 \\
 		//dir, _ := os.Getwd()
 		//filePath := dir + "\\" + fileName
 		dir := os.Args[2]
@@ -61,11 +61,11 @@ func handlerPostRequest(request *http.Request, conn net.Conn) {
 }
 
 func handlerGetRequest(request *http.Request, conn net.Conn) {
-	switch path := request.URL.Path; path {
-	case "/":
+	switch path := request.URL.Path; {
+	case path == "/":
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		return
-	case "/user-agent":
+	case path == "/user-agent":
 		head := request.Header
 		value := head.Get("User-Agent")
 		length := len(value)
@@ -73,26 +73,24 @@ func handlerGetRequest(request *http.Request, conn net.Conn) {
 		res := StatusOK + ContentTypeText + CRLF + contentLength + CRLF + CRLF + value
 		conn.Write([]byte(res))
 		return
-	}
 
-	if strings.Contains(request.URL.Path, "/files") {
+	case strings.Contains(path, "/files"):
 		// /files/foo
 		url := request.URL.Path
 		fileName := strings.TrimPrefix(url, "/files/")
+		// 这里的路径是由参数带进来的
 		dir := os.Args[2]
-		fmt.Println("fileName: ", fileName)
-		fmt.Println("dir+fileName: ", dir+fileName)
+		//fmt.Println("fileName: ", fileName)
+		//fmt.Println("dir+fileName: ", dir+fileName)
 		data, err := os.ReadFile(dir + fileName)
-		// 可能是路径问题
 		if err != nil {
 			fmt.Println("err: ", err)
 			conn.Write([]byte(Status404 + CRLF))
 			return
 		}
-		fmt.Println("success")
 		res := StatusOK + ContentTypeOctet + CRLF + ContentLength + strconv.Itoa(len(data)) + CRLF + CRLF + string(data)
 		conn.Write([]byte(res))
-	} else if strings.Contains(request.URL.Path, "/echo") {
+	case strings.Contains(path, "/echo"):
 		// /echo/{str}
 		path := request.URL.Path
 		str := strings.Split(path, "/")[2]
@@ -101,40 +99,29 @@ func handlerGetRequest(request *http.Request, conn net.Conn) {
 		contentLength := ContentLength + strconv.Itoa(length) + CRLF
 		// 获取 accept-encoding
 		encodingStr := request.Header.Get("accept-encoding")
-		fmt.Println("encodingStr: ", encodingStr)
+		//fmt.Println("encodingStr: ", encodingStr)
 
 		var res string
 		if checkValidEncoding(encodingStr) {
-			// 这里的压缩数据 别人的代码能过，但是我的过不了 奇怪
-			// 压缩数据
 			var buf bytes.Buffer
 			writer := gzip.NewWriter(&buf)
 			writer.Write([]byte(str))
 			writer.Close()
 			content := buf.String()
-			//content := str
-			//var buffer bytes.Buffer
-			//w := gzip.NewWriter(&buffer)
-			//w.Write([]byte(content))
-			//w.Close()
-			//content = buffer.String()
-
 			res = StatusOK + ContentEncoding + "gzip" + CRLF + contentType + ContentLength + fmt.Sprint(len(content)) + CRLF + CRLF + content
 		} else {
 			res = StatusOK + contentType + contentLength + CRLF + str
 		}
-
-		fmt.Println("res: ", res)
+		//fmt.Println("res: ", res)
 		conn.Write([]byte(res))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	default:
+		conn.Write([]byte(Status404 + CRLF))
 	}
 }
 
 func checkValidEncoding(encodingStr string) bool {
 	encodingArr := strings.Split(encodingStr, ", ")
-	fmt.Println("encodingArr: ", encodingArr)
-
+	//fmt.Println("encodingArr: ", encodingArr)
 	for _, value := range encodingArr {
 		if value == "gzip" {
 			return true
@@ -163,7 +150,5 @@ func main() {
 		}
 		// 并发处理 请求
 		go Handler(conn)
-		//还可以直接用 conn.Read 方法来做解析，先读进来,再用 "\r\n" 切分
 	}
-
 }
